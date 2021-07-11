b96d8f2e583af880a98a2c21b87365f154155f86
Itai Danielli <Danielli.Itai@gmail.com>
2021-07-11 19:37:33 +0300

    Add GUI tests and rename console api commands file.

--- CommandsTest/CommandsAppTest.cs
+++ CommandsTest/CommandsAppTest.cs
@@ -11,17 +11,8 @@ namespace CshConsoleTest
 	public class CommandsTest
 	{
 		public const string CMD_ECHO_PARAM= "hello echo";
-		public const string CMD_LINE_ECHO= "echo(hello world)";
+		public const string CMD_LINE_ECHO= "echo(" + CMD_ECHO_PARAM + ")";
 
-		private static bool CommandEcho(string[] parameters)
-		{
-			return (true);
-		}
-
-		[TestMethod]
-		public void TestMethod1()
-		{
-		}
 
 		[TestMethod]
 		public void TestParser()
@@ -35,14 +26,6 @@ namespace CshConsoleTest
 			return;
 		}
 
-		//	Test the echo command function.
-		[TestMethod]
-		public void	TestEcho()
-		{
-			string[] parameters = CommandsApi.CommandParams(CMD_LINE_ECHO);
-			Assert.IsTrue(AppCommands.CommandEcho(parameters));
-			return;
-		}
 
 		//	Test the commands list using the echo command function.
 		[TestMethod]
@@ -53,10 +36,10 @@ namespace CshConsoleTest
 			Assert.IsNotNull(pCommands);
 
 			//	Check adding echo command to commands list.
-			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_ECHO, CommandEcho));
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_ECHO, AppCommands.CommandEcho));
 
 			//	Execute the echo command in the command list.
-			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_ECHO));
+			Assert.IsTrue(CMD_ECHO_PARAM == CommandsApi.CommandExec(ref pCommands, CMD_LINE_ECHO));
 			return;
 		}
 	};
---
+++ CommandsTest/CommandsGuiTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using CshConsoleAPI;
+
+
+
+namespace CshConsoleTest
+{
+
+	[TestClass]
+	public class CommandsGuiTest
+	{
+		private const string CMD_GUI_SHOW = DialogCommands.GUI_SHOW + "()";
+		private const string CMD_GUI_MULT = DialogCommands.GUI_MULT + "(2,3)";
+		private const string CMD_GUI_CLOSE = DialogCommands.GUI_CLOSE + "()";
+
+
+
+
+		//	Test the commands list using the echo command function.
+		[TestMethod]
+		public void TestCommandGui()
+		{
+			//	Check creating a new commands list.
+			Commands pCommands = CommandsApi.CommandsInit();
+			Assert.IsNotNull(pCommands);
+
+			//	Check adding echo command to commands list.
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, DialogCommands.GUI_SHOW, DialogCommands.CommandGuiShow));
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, DialogCommands.GUI_MULT, DialogCommands.CommandGuiMult));
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, DialogCommands.GUI_CLOSE, DialogCommands.CommandGuiClose));
+
+			//	Execute the echo command in the command list.
+			//Assert.IsTrue(AppCommands.CMD_TRUE == CommandsApi.CommandExec(ref pCommands, CMD_GUI_SHOW));
+			Assert.IsTrue("6" == CommandsApi.CommandExec(ref pCommands, CMD_GUI_MULT));
+			Assert.IsTrue(AppCommands.CMD_OK == CommandsApi.CommandExec(ref pCommands, CMD_GUI_CLOSE));
+
+			return;
+		}
+
+	};
+}
